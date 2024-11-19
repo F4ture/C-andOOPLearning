d19fbeb8d98eeffcc8d8cced33d6bce0e59e2ee5
F4ture <71616413+F4ture@users.noreply.github.com>
2024-11-19 17:58:18 +1000

    KT35
    KT35

---
+++ Test123321/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test123321
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ready_Click(object sender, RoutedEventArgs e)
+        {
+            if (rButton13.IsChecked == true || rButton14.IsChecked == true || rButton15.IsChecked == true || rButton16.IsChecked == true)
+            {
+                if (rButton8.IsChecked == true && rButton9.IsChecked == true && rButton18.IsChecked == true)
+                {
+                    HaHaHa.Text = "Ну а всё таки что насчёт 100 баллов?";
+                    Answer.Text = "Победа, а теперь посмотри на 3ий вопрос";
+                }
+                else
+                {
+                    HaHaHa.Text = "Ну а всё таки что насчёт 100 баллов?";
+                    Answer.Text = "Проигрышь, а теперь посмотри на 3ий вопрос";
+                }
+            }
+            else
+            {
+                Answer.Text = "Ответь на важное";
+            }
+        }
+    }
+}
